f5bb7862f310435ecd8e255b30135b719d4d85c4
Kostya <intsyn@mail.ru>
2020-05-08 22:42:41 +0300

    move joystic to additions

---
+++ Assets/Additions/Joystick Pack/Scripts/Joysticks/JoysticController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JoysticsEnum { Fixed, Floating}
+
+public class JoysticController : MonoBehaviour
+{
+    //ToDo сделать переключение джойтиков и событие по переключению
+    public Joystick currentJoystick;
+    public JoysticsEnum currentType = JoysticsEnum.Floating;
+    public bool visible { get { return currentJoystick.gameObject.active;  } set { currentJoystick.gameObject.SetActive(value); } }
+
+    public void SwithJoystic(JoysticsEnum joystics)
+    {
+        //visible = false;
+        currentType = joystics;
+        switch (joystics)
+        {
+            case JoysticsEnum.Fixed:
+                currentJoystick = transform.Find("Fixed Joystick").GetComponent<Joystick>();
+                break;
+            case JoysticsEnum.Floating:
+                currentJoystick = transform.Find("Floating Joystick").GetComponent<Joystick>();
+                break;
+        }
+        //visible = true;
+    }
+}
